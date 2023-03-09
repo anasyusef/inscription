18f52dec3c02be631e7c2ea0ff182ac2b308bbe2
Anas Yousef <anas@basementapp.xyz>
2023-03-09 18:23:38 -0400

    add twitter

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
-import { siteConfig } from "@/config/site"
 
+import { siteConfig } from "@/config/site"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
@@ -13,8 +13,8 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            {/* <Link
-              href={siteConfig.links.github}
+            <Link
+              href={siteConfig.links.twitter}
               target="_blank"
               rel="noreferrer"
             >
@@ -25,10 +25,10 @@ export function SiteHeader() {
                   className: "text-slate-700 dark:text-slate-400",
                 })}
               >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
+                <Icons.twitter fill="currentColor" className="h-5 w-5" />
+                <span className="sr-only">Twitter</span>
               </div>
-            </Link> */}
+            </Link>
             <Link
               href={siteConfig.links.discord}
               target="_blank"
